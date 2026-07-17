0d0b2a62ecceb3a4bee98225dbb544334cba0804
Kyle Brennan <kyle@ona.com>
2026-07-17 17:51:55 +0000

    fix: use public API client for environment class query

--- internal/provider/environment_class_query_test.go
+++ internal/provider/environment_class_query_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"testing"
 
-	v1 "github.com/gitpod-io/terraform-provider-ona/internal/api/go/v1"
+	v1 "github.com/gitpod-io/terraform-provider-ona/api/public-clients/go/v1"
 	"github.com/google/go-cmp/cmp"
 	testresource "github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/querycheck"
